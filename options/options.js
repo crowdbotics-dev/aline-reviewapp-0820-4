0902722aca5f9d8f39d86a0d8352f451d928770c
Crowdbotics <engineering@crowdbotics.com>
2021-08-20 15:43:16 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,8 @@
+import { StyleSheet } from "react-native";
+
+export const globalOptions = {
+  name: "aline_reviewapp_0820_4Identifier",
+  url: "https://aline_reviewapp_0820_4Identifier.botics.co",
+  api: "https://aline_reviewapp_0820_4Identifier.botics.co/api/v1"
+}
+export const modulesOptions = {}
